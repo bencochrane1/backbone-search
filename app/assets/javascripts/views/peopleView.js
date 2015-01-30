e47ca425503eb167845c34834011325cc7406cae
Ben Cochrane <bencochrane1@me.com>
2015-01-30 16:33:49 +1100

    add in crud for list

--- app/assets/javascripts/views/peopleView.js
+++ app/assets/javascripts/views/peopleView.js
@@ -3,13 +3,43 @@ var App =  App || {}
 App.PeopleView = Backbone.View.extend({
 
     events: {
-        'keyup input': 'search'
+        'keyup input.main-entry': 'search',
+        // 'click li': 'addInName',
+        'click button': 'deletePerson',
+        'dblclick li': 'renderForm', 
+        'submit form': 'updatePerson'
+    },
+
+    updatePerson: function(e) {
+        e.preventDefault();
+        var firstNameData = $(e.currentTarget).find(".first_name").val();
+        var lastNameData = $(e.currentTarget).find(".last_name").val();
+        var id = $(e.currentTarget).parent().attr("data-id");
+        var data = this.collection.get(id);
+        data.save({
+            first_name: firstNameData,
+            last_name: lastNameData
+        });
+        $(e.currentTarget).fadeOut();
+        this.search();
+    },
+
+    renderForm: function(e) {
+        var id = $(e.currentTarget).attr("data-id");
+        var data = this.collection.get(id);
+        $(e.currentTarget).html(JST['form']( data.toJSON() ));
+
+    },
+
+    deletePerson: function(e) {
+        var id = $(e.currentTarget).parent().attr("data-id");
+        e.stopPropagation();
+        $(e.currentTarget).parent().fadeOut();
+        this.collection.get(id).destroy();
     },
 
     render: function() {
-        this.$el.html(
-            JST['app']()
-        );
+        this.$el.html(JST['app']());
         this.renderCollection(this.collection.toJSON());
         return this;       
     },
@@ -27,7 +57,20 @@ App.PeopleView = Backbone.View.extend({
             var searchdata = this.collection.filterBySearch(searchText).toJSON();
             this.renderCollection(searchdata);
         }
-    }
+    },
 
+    addInName: function(e) {
+        var id = $(e.currentTarget).attr("data-id");
+        var textForFilling = this.collection.get(id).fullName();
+
+        this.$el.find('input').val(textForFilling);
+        this.search();
+        // clickedList = this.$el.find(e.currentTarget).text();
+        // this.$el.find('input').val(clickedList);
+    },
+
+    renderSearchBox: function() {
+        this.$el.find('#inputForm').html(JST['inputbox']());
+    }
     
 });
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,32 @@
 class PeopleController < ApplicationController
+
+    # before_action :person, only: [:update, :destroy]
+
     def index
         render json: Person.all
     end
+
+    def create
+        render json: Person.create(people_params) 
+    end
+    
+    def update
+        @person = Person.find(params[:id])
+        @person.update(people_params)
+        render json: @person
+    end
+
+    def destroy
+        @person = Person.find(params[:id])
+        @person.destroy
+
+        render json: { status: 'ok' }
+    end
+
+    private
+
+    def people_params
+        params.require(:person).permit(:first_name, :last_name)
+    end
+
 end
