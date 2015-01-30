d56cd994d5e02d7e50d8ba458e30d4f1d0849b45
Ben Cochrane <bencochrane1@me.com>
2015-01-30 17:14:52 +1100

    update peopleView and personView with simpler code

--- app/assets/javascripts/views/peopleView.js
+++ app/assets/javascripts/views/peopleView.js
@@ -1,76 +1,48 @@
-var App =  App || {}
+var App = App || {};
 
 App.PeopleView = Backbone.View.extend({
 
-    events: {
-        'keyup input.main-entry': 'search',
-        // 'click li': 'addInName',
-        'click button': 'deletePerson',
-        'dblclick li': 'renderForm', 
-        'submit form': 'updatePerson'
-    },
+  events: {
+    'keyup .search_input': 'search',
+    // 'click li': 'fillSearch',
+  },
 
-    updatePerson: function(e) {
-        e.preventDefault();
-        var firstNameData = $(e.currentTarget).find(".first_name").val();
-        var lastNameData = $(e.currentTarget).find(".last_name").val();
-        var id = $(e.currentTarget).parent().attr("data-id");
-        var data = this.collection.get(id);
-        data.save({
-            first_name: firstNameData,
-            last_name: lastNameData
-        });
-        $(e.currentTarget).fadeOut();
-        this.search();
-    },
+  renderCollection: function(data) {
+    this.$el.find("ul").html("");
 
-    renderForm: function(e) {
-        var id = $(e.currentTarget).attr("data-id");
-        var data = this.collection.get(id);
-        $(e.currentTarget).html(JST['form']( data.toJSON() ));
+    data.each(function(person) {
+      var personView = new App.PersonView({ model: person });
+      this.$el.find("ul").append(personView.render().el);
+    }, this);
+  },
 
-    },
+  render: function() {
 
-    deletePerson: function(e) {
-        var id = $(e.currentTarget).parent().attr("data-id");
-        e.stopPropagation();
-        $(e.currentTarget).parent().fadeOut();
-        this.collection.get(id).destroy();
-    },
+    this.$el.html(JST['app']());
 
-    render: function() {
-        this.$el.html(JST['app']());
-        this.renderCollection(this.collection.toJSON());
-        return this;       
-    },
+    this.renderCollection(this.collection);
 
-    renderCollection: function(data) {
-        this.$el.find('ul').html(JST['people']({ people: data }));
-        return this;
-    },
+    return this;
+  },
 
-    search: function() {
-        searchText = this.$el.find('input').val();
-        if (searchText == "") {
-            this.renderCollection(this.collection.toJSON());
-        } else {
-            var searchdata = this.collection.filterBySearch(searchText).toJSON();
-            this.renderCollection(searchdata);
-        }
-    },
+  search: function() {
+    var searchText = this.$el.find("input").val();
 
-    addInName: function(e) {
-        var id = $(e.currentTarget).attr("data-id");
-        var textForFilling = this.collection.get(id).fullName();
-
-        this.$el.find('input').val(textForFilling);
-        this.search();
-        // clickedList = this.$el.find(e.currentTarget).text();
-        // this.$el.find('input').val(clickedList);
-    },
-
-    renderSearchBox: function() {
-        this.$el.find('#inputForm').html(JST['inputbox']());
+    if (searchText === "") {
+      this.renderCollection(this.collection);
+    } else {
+      this.renderCollection(
+        this.collection.filterBySearch(searchText)
+      );
     }
-    
+  }
+
 });
+
+
+
+
+
+
+
+
--- app/assets/javascripts/views/personView.js
+++ app/assets/javascripts/views/personView.js
@@ -1,6 +1,43 @@
-var App =  App || {}
+var App = App || {};
 
-App.Person = Backbone.View.extend({
-    tagName: 'div'
+App.PersonView = Backbone.View.extend({
+  tagName: 'li',
 
+  events: {
+    'click button': 'deletePerson',
+    'dblclick' : 'renderForm', 
+    'submit form' : 'updatePerson'  
+  },
+
+  render: function() {
+    this.$el.html(JST['person'](this.model.toJSON()));
+
+    return this;
+  },
+
+  deletePerson: function(event) {
+    event.stopPropagation();
+
+    this.$el.fadeOut();
+    this.model.destroy();
+  },
+
+  renderForm: function(event) {
+    this.$el.html(JST['form'](
+      this.model.toJSON()
+    ));
+  },
+
+  updatePerson: function(event) {
+    event.preventDefault();
+
+    this.model.save({
+      first_name: this.$el.find(".first_name").val(),
+      last_name: this.$el.find(".last_name").val()
+    });
+
+    this.$el.find("form").fadeOut();
+
+    this.render();
+  }
 });
